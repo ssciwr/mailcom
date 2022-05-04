2e9a5b93836cc0980dc5722379cbe3b3ec74dc9e
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2022-05-04 14:42:42 +0200

    Input 2 (#2)
    * improved folder structure
    
    * removing header
    
    * added more structure to repo and some initial spacy routines
    
    * deleting header, still need to fix email address

--- src/anonymizer/in_out.py
+++ src/anonymizer/in_out.py
@@ -1,36 +1,43 @@
 import os
-import email
 from email import policy
 from email.parser import BytesHeaderParser, BytesParser
-from cv2 import fillPoly
-import pandas as pd
+
+path = './data/'
 
 def list_of_files(path):
     """Get all the eml files in the directory and put them in a list."""
     email_list = [f for f in os.listdir(path) if f.endswith('.eml')]
     return email_list
 
-path = './data/'
-eml_files = list_of_files(path)
-
-print("Found files: {}".format(eml_files))
-
-keys_to_delete = ["From", "To", "Date", "Subject"]
-for file in eml_files:
-    with open(path+file, 'rb') as fp:
-        name = fp.name  
+def get_text(name):
+    with open(name, 'rb') as fp:
         msg = BytesParser(policy=policy.default).parse(fp)
-        # clean the header information
-        for mykey in keys_to_delete:
-            if mykey not in msg.keys():
-                print("Could not delete key {}".format(mykey))
-                print("Not one of {}".format(msg.keys()))
-            msg.__delitem__(mykey)
-        text = msg.get_body(preferencelist='plain').get_content()
-        print(text)
-        with open("{}.out".format(name), "w") as file:
-            file.write()
-    fp.close()
+    return msg.get_body(preferencelist='plain').get_content()
 
-# df_eml = pd.DataFrame([file_names, texts]).T
-# df_eml.columns = ['file_name', 'text']
+def delete_header(text):
+    items_to_delete = ["Von:", "Gesendet:", "Betreff:", "An:", "Cc:",
+                       "Sujet", "Date", "De", "Pour", "Copie "]
+    lines_to_delete = []
+    text_out_list = text.splitlines()
+    for index, line in enumerate(text_out_list):
+        if any(i in items_to_delete for i in line.strip().split(" ")):
+            lines_to_delete.append(index)
+            print("Deleting {}".format(line))
+    # delete lines
+    for i in reversed(lines_to_delete):
+        # print("xxxxx {}".format(text_out_list[i]))
+        del text_out_list[i]
+    print(" ".join(text_out_list))
+    return " ".join(text_out_list)
+
+def write_file(text, name):
+    with open("{}.out".format(name), "w") as file:
+        file.write(text)
+
+if __name__ == "__main__":
+    eml_files = list_of_files(path)
+    print("Found files: {}".format(eml_files))
+    for file in eml_files:
+        text = get_text(path+file)
+        text = delete_header(text)
+        # still need to delete email addresses in "blabla wrote:"
