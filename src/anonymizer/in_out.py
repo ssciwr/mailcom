73cd36909a4296e5a7753c2953380ae595959f23
iulusoy <inga.ulusoy@uni-heidelberg.de>
2022-05-05 12:20:27 +0200

    linting

--- src/anonymizer/in_out.py
+++ src/anonymizer/in_out.py
@@ -1,22 +1,35 @@
 import os
 from email import policy
-from email.parser import BytesHeaderParser, BytesParser
+from email.parser import BytesParser
 
-path = './data/'
+path = "./data/"
+
 
 def list_of_files(path):
     """Get all the eml files in the directory and put them in a list."""
-    email_list = [f for f in os.listdir(path) if f.endswith('.eml')]
+    email_list = [f for f in os.listdir(path) if f.endswith(".eml")]
     return email_list
 
+
 def get_text(name):
-    with open(name, 'rb') as fp:
+    with open(name, "rb") as fp:
         msg = BytesParser(policy=policy.default).parse(fp)
-    return msg.get_body(preferencelist='plain').get_content()
+    return msg.get_body(preferencelist="plain").get_content()
+
 
 def delete_header(text):
-    items_to_delete = ["Von:", "Gesendet:", "Betreff:", "An:", "Cc:",
-                       "Sujet", "Date", "De", "Pour", "Copie "]
+    items_to_delete = [
+        "Von:",
+        "Gesendet:",
+        "Betreff:",
+        "An:",
+        "Cc:",
+        "Sujet",
+        "Date",
+        "De",
+        "Pour",
+        "Copie ",
+    ]
     lines_to_delete = []
     text_out_list = text.splitlines()
     for index, line in enumerate(text_out_list):
@@ -29,14 +42,16 @@ def delete_header(text):
         del text_out_list[i]
     return " ".join(text_out_list)
 
+
 def write_file(text, name):
     with open("{}.out".format(name), "w") as file:
         file.write(text)
+
 
 if __name__ == "__main__":
     eml_files = list_of_files(path)
     print("Found files: {}".format(eml_files))
     for file in eml_files:
-        text = get_text(path+file)
+        text = get_text(path + file)
         text = delete_header(text)
         # still need to delete email addresses in "blabla wrote:"
--- src/anonymizer/parse.py
+++ src/anonymizer/parse.py
@@ -1,4 +1,3 @@
-from re import I
 import spacy as sp
 import stanza as sa
 import in_out as in_out
@@ -6,24 +5,27 @@ import in_out as in_out
 lang = "es"
 path = "./data/test/"
 
+
 def get_sentences(doc):
     # we are only interested in first and last sentence
     # remove any named entities from first and last sentence
-    i = 0
     # spacy
-    # unfortunately need to iterate through sents the way the 
+    # unfortunately need to iterate through sents the way the
     # attribute is defined in spacy
     text = []
     for sent in doc.sents:
         text.append(str(sent))
     return text
-        
+
+
 def process_doc(doc):
     # stanza
     for sent in doc.sentences:
         entlist = [ent.text for ent in sent.ents]
         wordlist = [word.text for word in sent.words]
         # now remove all entlist strings from wordlist
+    return wordlist, entlist
+
 
 def init_spacy(lang):
     if lang == "es":
@@ -39,7 +41,9 @@ def init_spacy(lang):
     # initialize nlp pipeline
     try:
         # disable not needed components
-        nlp = sp.load(model, exclude = ['morphologizer', 'attribute_ruler', 'lemmatizer', 'ner'])
+        nlp = sp.load(
+            model, exclude=["morphologizer", "attribute_ruler", "lemmatizer", "ner"]
+        )
     except OSError:
         raise OSError("Could not find {} in standard directory.".format(model))
     nlp = sp.load(model)
@@ -48,9 +52,13 @@ def init_spacy(lang):
     # print("Loading components {} from {}.".format(components, model))
     return nlp
 
+
 def init_stanza(lang):
-    nlp = sa.Pipeline(lang=lang, processors='tokenize,mwt,pos,lemma,ner', tokenize_no_ssplit=True)
+    nlp = sa.Pipeline(
+        lang=lang, processors="tokenize,mwt,pos,lemma,ner", tokenize_no_ssplit=True
+    )
     return nlp
+
 
 if __name__ == "__main__":
     nlp_spacy = init_spacy(lang)
@@ -58,11 +66,11 @@ if __name__ == "__main__":
     # process the text
     eml_files = in_out.list_of_files(path)
     for file in eml_files:
-        text = in_out.get_text(path+file)
+        text = in_out.get_text(path + file)
         text = in_out.delete_header(text)
         doc_spacy = nlp_spacy(text)
         text = get_sentences(doc_spacy)
         # start with first line
         # we still need to remove lines with "@" as well
         doc_stanza = nlp_stanza(text[0])
-        process_doc(doc_stanza)
+        wordlist, entlist = process_doc(doc_stanza)
