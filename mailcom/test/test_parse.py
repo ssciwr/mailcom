c6121c4e2b88a967793a97190e09e9aeed844fda
Felix <65565033+fexfl@users.noreply.github.com>
2024-10-15 14:36:39 +0200

    Added test for reset

--- mailcom/test/test_parse.py
+++ mailcom/test/test_parse.py
@@ -1,5 +1,11 @@
-from mailcom import parse
+from mailcom import parse, inout
 import pytest
+from pathlib import Path
+from importlib import resources
+
+pkg = resources.files("mailcom")
+
+FILE_PATH = Path(pkg / "test" / "data")
 
 
 # these worked when we were using strings
@@ -27,8 +33,43 @@ def get_instant():
     return parse.Pseudonymize()
 
 
+@pytest.fixture()
+def get_default_fr():
+    inst = parse.Pseudonymize()
+    inst.init_spacy("fr")
+    inst.init_transformers()
+    return inst
+
+
+@pytest.fixture()
+def get_sample_texts():
+    inst = inout.InoutHandler(FILE_PATH)
+    inst.list_of_files()
+    email_list = []
+    for file in inst.email_list:
+        text = inst.get_text(file)
+        text = inst.get_html_text(text)
+        if not text:
+            continue
+        email_list.append(text)
+    return email_list
+
+
 def test_init_spacy(get_instant):
     with pytest.raises(KeyError):
         get_instant.init_spacy("not_a_language")
     with pytest.raises(OSError):
         get_instant.init_spacy("fr", "not_an_existing_spacy_model")
+
+
+# TODO init_transformers
+
+
+def test_reset(get_default_fr, get_sample_texts):
+    for text in get_sample_texts:
+        # Test that used names lists are empty
+        # They should be cleared after every email
+        assert len(get_default_fr.used_first_names) == 0
+        assert len(get_default_fr.used_last_names) == 0
+        # pseudonymize email
+        get_default_fr.pseudonymize(text)
