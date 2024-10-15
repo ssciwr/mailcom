99a562d609558485cf9fb6cbc47de75939d70d52
Felix <65565033+fexfl@users.noreply.github.com>
2024-10-15 14:10:41 +0200

    Added test for init_spacy

--- mailcom/test/test_parse.py
+++ mailcom/test/test_parse.py
@@ -1,4 +1,4 @@
-from mailcom.parse import make_dir, check_dir
+from mailcom import parse
 import pytest
 
 
@@ -6,16 +6,29 @@ import pytest
 # with the update to Path, we need to change the tests
 def test_check_dir(tmpdir):
     mydir = tmpdir.mkdir("sub")
-    assert check_dir(str(mydir))
+    assert parse.check_dir(str(mydir))
     with pytest.raises(OSError):
-        check_dir(str(tmpdir.join("sub2")))
+        parse.check_dir(str(tmpdir.join("sub2")))
 
 
 def test_make_dir(tmpdir):
     mydir = tmpdir.join("sub")
-    make_dir(str(mydir))
+    parse.make_dir(str(mydir))
     assert mydir.check()
+
 
 def test_check_dir_fail():
     with pytest.raises(OSError):
-        check_dir(str("mydir"))
+        parse.check_dir(str("mydir"))
+
+
+@pytest.fixture()
+def get_instant():
+    return parse.Pseudonymize()
+
+
+def test_init_spacy(get_instant):
+    with pytest.raises(KeyError):
+        get_instant.init_spacy("not_a_language")
+    with pytest.raises(OSError):
+        get_instant.init_spacy("fr", "not_an_existing_spacy_model")
