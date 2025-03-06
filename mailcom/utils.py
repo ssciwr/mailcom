167ffc0842fd919b36b3a0f2505989a3a3934c90
Tuyen Le <lethikimtuyen87@gmail.com>
2025-03-06 15:07:46 +0100

    feat: add support for language detection with langid and langdetect (#45)

--- mailcom/utils.py
+++ mailcom/utils.py
@@ -1,4 +1,7 @@
 import os
+import langid
+from langid.langid import LanguageIdentifier, model
+from langdetect import detect_langs, DetectorFactory
 
 
 def check_dir(path: str) -> bool:
@@ -11,3 +14,50 @@ def check_dir(path: str) -> bool:
 def make_dir(path: str):
     # make directory at path
     os.makedirs(path + "/")
+
+
+class LangDetector:
+    def __init__(self):
+        self.lang_id = LanguageIdentifier.from_modelstring(model, norm_probs=True)
+        self.detect_langs = detect_langs
+
+    def constrain_langid(self, lang_set=[]):
+        """Set constraint for language set of langid. Default is no constrained languages.
+        """
+        if lang_set:
+            lang_intersec = list(set(lang_set) & set(self.lang_id.nb_classes))
+            if lang_intersec:
+                self.lang_id.set_languages(lang_intersec)
+            else:
+                raise ValueError(
+                    "No languages in the set are supported by langid. Please check the language set."
+                )
+    
+    def determ_langdetect(self):
+        """Enforce consistent results for langdetect.
+        """
+        DetectorFactory.seed = 0
+
+    def detect_with_langid(self, text: str):
+        """Dectect language of a given text using langid library.
+
+        Args:
+            text (str): The text to detect the language of.
+
+        Returns:
+            lang (str): The detected language.
+            prob (float): The probability of the detection.
+        """
+        lang, prob = self.lang_id.classify(text)
+        return lang, prob
+    
+    def detect_with_langdetect(self, text: str):
+        """Dectect language of a given text using langdetect library.
+
+        Args:
+            text (str): The text to detect the language of.
+
+        Returns:
+            list(str, float): The possible language and their probabilities.
+        """
+        return detect_langs(text)
