9677a5fab6fb58b1d0e298e0e9433925734cdb4d
Tuyen Le <lethikimtuyen87@gmail.com>
2025-03-11 08:47:25 +0100

    feat: initial commit of the branch for issue #49

--- mailcom/test/test_utils.py
+++ mailcom/test/test_utils.py
@@ -2,6 +2,7 @@ import math
 from mailcom import utils
 import pytest
 from string import punctuation
+import datetime
 
 
 def test_check_dir(tmpdir):
@@ -544,3 +545,53 @@ def test_detect_lang_sentences_trans(get_lang_det_w_init, get_mixed_lang_docs):
                     assert detected_lang == get_mixed_lang_docs[doc][i]
             else:
                 assert detected_lang == get_mixed_lang_docs[doc][i]
+
+
+@pytest.fixture()
+def get_time_detector():
+    return utils.TimeDetector()
+
+
+sample_dates = {
+    "absolute": {
+        "2025-03-10": datetime.datetime(2025, 3, 10, 0, 0),
+        "2025-03-10 12:15:20": datetime.datetime(2025, 3, 10, 12, 15, 20),
+        "May 10, 2025": datetime.datetime(2025, 5, 10, 0, 0),
+        "15.03.2025": datetime.datetime(2025, 3, 15, 0, 0),
+    },
+    "relative": {
+        "tomorrow": datetime.date.today() + datetime.timedelta(days=1),
+        "today": datetime.date.today(),
+        "two weeks ago": datetime.date.today() - datetime.timedelta(days=14),
+        "in two weeks": datetime.date.today() + datetime.timedelta(days=14),
+        "2 weeks ago": datetime.date.today() - datetime.timedelta(days=14),
+    },
+    "invalid": ["2025-13-15", "2025-23-17 25:15:20"],
+    "confusing": {
+        "10.03.2025": datetime.datetime(2025, 10, 3, 0, 0),
+        "2025-15-10": datetime.datetime(2025, 10, 15, 0, 0),
+    },
+}
+
+
+def test_parse_time(get_time_detector):
+    for date_str, date_obj in sample_dates["absolute"].items():
+        assert get_time_detector.parse_time(date_str) == date_obj
+    for date_str, date_obj in sample_dates["relative"].items():
+        assert get_time_detector.parse_time(date_str).date() == date_obj
+    for date_str in sample_dates["invalid"]:
+        assert get_time_detector.parse_time(date_str) is None
+    for date_str, date_obj in sample_dates["confusing"].items():
+        assert get_time_detector.parse_time(date_str) == date_obj
+
+
+def test_find_dates(get_time_detector):
+    extra_info = "The date in the email is: "
+    for date_str, date_obj in sample_dates["absolute"].items():
+        assert get_time_detector.find_dates(extra_info + date_str) == [date_obj]
+    # for date_str, date_obj in sample_dates["relative"].items():
+    #     assert get_time_detector.find_dates(extra_info + date_str) == [date_obj]
+    for date_str in sample_dates["invalid"]:
+        assert get_time_detector.find_dates(extra_info + date_str) == []
+    # for date_str, date_obj in sample_dates["confusing"].items():
+    #     assert get_time_detector.find_dates(extra_info + date_str) == [date_obj]
--- mailcom/utils.py
+++ mailcom/utils.py
@@ -6,6 +6,9 @@ from transformers import pipeline
 from pathlib import Path
 from mailcom.inout import InoutHandler
 import pandas as pd
+import dateparser
+import datefinder
+from datetime import datetime
 
 
 def check_dir(path: Path) -> bool:
@@ -248,6 +251,33 @@ class LangDetector:
 
         result_tree.addi(marked_idx, current_idx, current_lang)
         return result_tree
+
+
+class TimeDetector:
+    def __init__(self):
+        pass
+
+    def parse_time(self, text: str) -> datetime:
+        """Parse the time from text format to datetime format.
+
+        Args:
+            text (str): The text to parse the time from.
+
+        Returns:
+            datetime: The datetime object of the time parsed.
+        """
+        return dateparser.parse(text)
+
+    def find_dates(self, text: str) -> list[datetime]:
+        """Find dates in a given text.
+
+        Args:
+            text (str): The text to find dates in.
+
+        Returns:
+            list[str]: A list of dates found in the text.
+        """
+        return list(datefinder.find_dates(text))
 
 
 if __name__ == "__main__":
